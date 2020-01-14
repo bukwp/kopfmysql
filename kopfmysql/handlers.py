7a3d6f6beb76a5d9536b2021eb523a867bd4cdfb
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-14 01:14:36 +0100

    Update handlers.py

--- kopfmysql/handlers.py
+++ kopfmysql/handlers.py
@@ -10,9 +10,8 @@ async def startup(logger, **kwargs):
     logger.info(f"Starting bukwp.kopfmysql/{VERSION}")
     await asyncio.sleep(1)
 
-@kopf.on.create('bukwp.kopfmysql', 'v1', 'accounts')
-def create_1(body, meta, spec, status, **kwargs):
 
+def main(body, meta, spec, status, **kwargs):
     try:
         kopf.info(body, reason="OK", message=f"Connecting to mysql at {spec['service']}")
         cnx = mysql.connector.connect(
@@ -25,20 +24,18 @@ def create_1(body, meta, spec, status, **kwargs):
     except Exception as err:
         kopf.info(body, reason="OK", message=f"Failed connecting to mysql at {spec['service']}, {err}")
     return {'job1-status': 100}
+
+
+@kopf.on.create('bukwp.kopfmysql', 'v1', 'accounts')
+def create_1(body, meta, spec, status, **kwargs):
+    return main(body, meta, spec, status, **kwargs)
 
 
 @kopf.on.update('bukwp.kopfmysql', 'v1', 'accounts')
 def update_1(body, meta, spec, status, **kwargs):
+    return main(body, meta, spec, status, **kwargs)
 
-    try:
-        kopf.info(body, reason="OK", message=f"Connecting to mysql at {spec['service']}")
-        cnx = mysql.connector.connect(
-            user='root',
-            password='password',
-            host=spec['service']
-        )
-        kopf.info(body, reason="OK", message=f"Connected to mysql at {spec['service']}")
-        cnx.close()
-    except Exception as err:
-        kopf.info(body, reason="OK", message=f"Failed connecting to mysql at {spec['service']}, {err}")
-    return {'job1-status': 100}
+
+@kopf.on.resume('bukwp.kopfmysql', 'v1', 'accounts')
+def resume_1(body, meta, spec, status, **kwargs):
+    return main(body, meta, spec, status, **kwargs)
